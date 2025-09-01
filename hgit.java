64d2c07170c0d0b32c93ca043f87ed32188502e1
davidgalarzagarcia <dgalarza2056@gmail.com>
2025-09-01 14:05:27 -0500

    Commit de prueba

--- hgit.java
+++ hgit.java
@@ -4,7 +4,7 @@ public class hgit{
 
 public static void main(String[] args) {
     System.out.println("hola git");
-    System.out.println("he modificado el fichero");
+    System.out.println("new he modificado el fichero con cambios");
 }
 
 }
